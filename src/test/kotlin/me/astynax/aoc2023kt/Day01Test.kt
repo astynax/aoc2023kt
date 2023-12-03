cdb86db86b95f13dfe4d664bf26d81278aa54d3a
astynax <astynax@users.noreply.github.com>
2023-12-03 17:02:02 +0400

    Clean up some tests

--- src/test/kotlin/me/astynax/aoc2023kt/Day01Test.kt
+++ src/test/kotlin/me/astynax/aoc2023kt/Day01Test.kt
@@ -1,40 +1,39 @@
 package me.astynax.aoc2023kt
 
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Day01Test {
   @Test
   fun `step1 on example`() {
-    assert(Day01.step1(
-      """|1abc2
-         |pqr3stu8vwx
-         |a1b2c3d4e5f
-         |treb7uchet"""
-        .trimMargin().lines()
-    ) == 142)
+    assertEquals(142, Day01.step1("""
+      1abc2
+      pqr3stu8vwx
+      a1b2c3d4e5f
+      treb7uchet""".trimIndent().lines()
+    ))
   }
 
   @Test
   fun `step1 on input`() {
-    assert(Day01.step1(Day01.input.value) == 53921)
+    assertEquals(53921, Day01.step1(Day01.input.value))
   }
 
   @Test
   fun `step2 on example`() {
-    assert(Day01.step2(
-      """|two1nine
-         |eightwothree
-         |abcone2threexyz
-         |xtwone3four
-         |4nineeightseven2
-         |zoneight234
-         |7pqrstsixteen"""
-        .trimMargin().lines()
-    ) == 281)
+    assertEquals(281, Day01.step2("""
+      two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen""".trimIndent().lines()
+    ))
   }
 
   @Test
   fun `step2 on input`() {
-    assert(Day01.step2(Day01.input.value) == 54676)
+    assertEquals(54676, Day01.step2(Day01.input.value))
   }
 }
--- src/test/kotlin/me/astynax/aoc2023kt/Day02Test.kt
+++ src/test/kotlin/me/astynax/aoc2023kt/Day02Test.kt
@@ -1,24 +1,24 @@
 package me.astynax.aoc2023kt
 
-import org.junit.jupiter.api.*
-
-import me.astynax.aoc2023kt.Day02.asGame
 import me.astynax.aoc2023kt.Day02.Take
+import me.astynax.aoc2023kt.Day02.asGame
+import me.astynax.aoc2023kt.Day02.input
 import me.astynax.aoc2023kt.Day02.step1
 import me.astynax.aoc2023kt.Day02.step2
-import me.astynax.aoc2023kt.Day02.input
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Day02Test {
   @Test
   fun `asGame on example`() {
-    assert(
+    assertEquals(
+      3 to listOf(
+        Take(20, 8, 6),
+        Take(4, 13, 5),
+        Take(1, 5, 0),
+      ),
       "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-        .asGame() == (
-          3 to listOf(
-            Take(20, 8, 6),
-            Take(4, 13, 5),
-            Take(1, 5, 0),
-          ))
+        .asGame()
     )
   }
 
@@ -34,31 +34,31 @@ class Day02Test {
     assert(!(Take(3, 4, 5) contains Take(4, 4, 5)))
   }
 
-  private val example =
-    """|Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-       |Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-       |Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-       |Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-       |Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
-      .trimMargin().lines().map { it.asGame() }
+  private val example = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+    .trimIndent().lines().map { it.asGame() }
 
   @Test
   fun `step1 on example`() {
-    assert(step1(example) == 8)
+    assertEquals(8, step1(example))
   }
 
   @Test
   fun `step1 on input`() {
-    assert(step1(input.value) == 2085)
+    assertEquals(2085, step1(input.value))
   }
 
   @Test
   fun `step2 on example`() {
-    assert(step2(example) == 2286)
+    assertEquals(2286, step2(example))
   }
 
   @Test
   fun `step2 on input`() {
-    assert(step2(input.value) == 79315)
+    assertEquals(79315, step2(input.value))
   }
 }
